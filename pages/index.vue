3774c8f904909853b45e8fd431df0f51ee3630b1
joeshim <shinmi1129@gmail.com>
2021-07-21 16:36:01 +0900

    index

--- pages/index.vue
+++ pages/index.vue
@@ -13,27 +13,35 @@ export default {
     login() {
       var provider = new firebase.auth.GoogleAuthProvider();
       firebase.auth()
-        .signInWithPopup(provider)
-        .then((result) => {
-          /** @type {firebase.auth.OAuthCredential} */
-          var credential = result.credential;
+      .signInWithPopup(provider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
 
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = credential.accessToken;
-          // The signed-in user info.
-          var user = result.user;
-          // ...
-          this.$router.push('/confirm')
-        }).catch((error) => {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          // ...
-        });
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+      firebase.auth()
+      .onAuthStateChanged(user => {
+      if (user) {
+        // ログイン後のページに遷移する
+        this.$router.push('/confirm')
+      } else {
+        // ログイン失敗。エラー処理など(通常はあり得ない？)
+      }
+    })
     },
   },
 };
