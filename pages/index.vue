bec1db4be4bcf261651c0c9911dc1368075a54c6
joeshim <shinmi1129@gmail.com>
2021-07-21 16:17:03 +0900

    index

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,7 @@ import firebase from "~/plugins/firebase";
 export default {
   methods: {
     login() {
+      const provider = new firebase.auth.GoogleAuthProvider()
       firebase.auth().signInWithRedirect(provider)
       .then(() => {
         this.$router.push('/confirm')
