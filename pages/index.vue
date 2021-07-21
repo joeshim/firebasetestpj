91ae8806841da449a768fbfdae73af843bc0236a
joeshim <shinmi1129@gmail.com>
2021-07-21 16:30:24 +0900

    index

--- pages/index.vue
+++ pages/index.vue
@@ -11,11 +11,29 @@ import firebase from "~/plugins/firebase";
 export default {
   methods: {
     login() {
-      const provider = new firebase.auth.GoogleAuthProvider()
-      firebase.auth().signInWithRedirect(provider)
-      .then(() => {
-        this.$router.push('/confirm')
-      })
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          /** @type {firebase.auth.OAuthCredential} */
+          var credential = result.credential;
+
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          // ...
+          then.$router.push('/confirm')
+        }).catch((error) => {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
     },
   },
 };
